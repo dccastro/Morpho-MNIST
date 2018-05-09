0476c87d550997079c554f48302c21e078cdf0c0
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2018-05-09 11:08:48 +0100

    Implement saving and loading gzipped MNIST file format

--- util.py
+++ util.py
@@ -1,7 +1,11 @@
+import gzip
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Ellipse
 from torch.autograd import Variable
+
+import idx
 
 
 def plot_digit(x, ax=None, title=None, **kwargs):
@@ -19,3 +23,14 @@ def plot_digit(x, ax=None, title=None, **kwargs):
 def plot_ellipse(x, y, angle, major, minor, ax, **kwargs):
     ax.add_patch(Ellipse(xy=(x, y), width=2 * major, height=2 * minor, angle=np.rad2deg(angle),
                          **kwargs))
+
+
+def save(data, path):
+    with gzip.open(path, 'wb') as f:
+        idx.save_uint8(data, f)
+
+
+def load(path):
+    with gzip.open(path, 'rb') as f:
+        data = idx.load_uint8(f)
+    return data
