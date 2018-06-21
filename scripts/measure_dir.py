306571c2713312e05a4bfc598b393c5644eeaede
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2018-06-21 16:22:56 +0100

    Overhaul script to measure MNIST data directories

---
+++ scripts/measure_dir.py
@@ -0,0 +1,33 @@
+import argparse
+import multiprocessing
+import os
+
+from morphomnist import measure, util
+
+
+def measure_dir(data_dir, pool):
+    for name in ['t10k', 'train']:
+        in_path = os.path.join(data_dir, name + "-images-idx3-ubyte.gz")
+        out_path = os.path.join(data_dir, name + "-morpho.csv")
+        print(f"Processing MNIST data file {in_path}...")
+        data = util.load(in_path)
+        df = measure.measure_batch(data, pool=pool, chunksize=1000)
+        df.to_csv(out_path, index_label='index')
+        print(f"Morphometrics saved to {out_path}")
+
+
+def main(data_dirs):
+    with multiprocessing.Pool() as pool:
+        for data_dir in data_dirs:
+            measure_dir(data_dir, pool)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('datadirs', nargs='+')
+    args = parser.parse_args()
+    print(args.datadirs)
+
+    assert all(os.path.exists(data_dir) for data_dir in args.datadirs)
+
+    main(args.datadirs)
