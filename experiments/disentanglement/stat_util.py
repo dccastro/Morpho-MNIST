91a3731640ef68bf97f80f9a385f87f52f09ffc0
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2018-08-09 18:03:40 +0100

    Rename conf -> control for partial correlations

--- experiments/disentanglement/stat_util.py
+++ experiments/disentanglement/stat_util.py
@@ -6,14 +6,14 @@ def correlation(x, y):
     return ((x - x.mean()) / x.std(0)).T @ ((y - y.mean(0)) / y.std(0)) / x.shape[0]
 
 
-def partial_correlation(x, y, conf):
-    res_x = sm.OLS(x, conf).fit().resid
-    res_y = sm.OLS(y, conf).fit().resid
+def partial_correlation(x, y, control):
+    res_x = sm.OLS(x, control).fit().resid
+    res_y = sm.OLS(y, control).fit().resid
     return correlation(res_x, res_y)
 
 
-def partial_correlation2(x, y, conf):
-    var = np.column_stack([y, x, conf])
+def partial_correlation2(x, y, control):
+    var = np.column_stack([y, x, control])
     prec = np.linalg.inv(var.T @ var)
     return -prec[0, 1] / np.sqrt(prec[0, 0] * prec[1, 1])
 
@@ -27,28 +27,28 @@ def compute_infogan_pcorr(phi, mu, gamma, metrics, cols):
         dv = metrics[col].values
         for cat in range(cat_dim):
             iv = phi_[:, cat]
-            conf = np.column_stack([
+            control = np.column_stack([
                 np.ones(iv.shape[0]),
                 mu,
                 gamma_
             ])
-            pcorr[c, cat] = partial_correlation2(iv, dv, conf)
+            pcorr[c, cat] = partial_correlation2(iv, dv, control)
         for cont in range(cont_dim):
             iv = mu[:, cont]
-            conf = np.column_stack([
+            control = np.column_stack([
                 phi_,
                 mu[:, :cont],
                 mu[:, cont + 1:],
                 gamma_
             ])
-            pcorr[c, cat_dim + cont] = partial_correlation2(iv, dv, conf)
+            pcorr[c, cat_dim + cont] = partial_correlation2(iv, dv, control)
         for bin in range(bin_dim):
             iv = gamma_[:, bin]
-            conf = np.column_stack([
+            control = np.column_stack([
                 phi_,
                 mu,
                 gamma_[:, :bin],
                 gamma_[:, bin + 1:]
             ])
-            pcorr[c, cat_dim + cont_dim + bin] = partial_correlation2(iv, dv, conf)
+            pcorr[c, cat_dim + cont_dim + bin] = partial_correlation2(iv, dv, control)
     return pcorr
