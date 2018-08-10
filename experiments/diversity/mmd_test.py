c7cc2afa270d787dffabd2345f0df3e6826a87a0
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2018-08-10 18:27:27 +0100

    Add MMD two-sample tests for test data vs. model samples

---
+++ experiments/diversity/mmd_test.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+from morphomnist.analysis import kernels, mmd
+
+if __name__ == '__main__':
+    cols = ['length', 'thickness', 'slant', 'width', 'height']
+    # cols = ['width', 'height']
+    test_metrics = pd.read_csv("/vol/biomedic/users/dc315/mnist/plain/t10k-morpho.csv")[cols]
+    specs = ["VAE-64_plain", "GAN-64_plain", "GAN-2_plain"]
+    N = 10000
+    np.set_printoptions(linewidth=160, precision=4)
+    seed = 0
+    print(seed)
+    for spec in specs:
+        print("Test data vs.", spec)
+        sample_metrics = pd.read_csv(f"/data/morphomnist/metrics/{spec}_metrics.csv")[cols]
+
+        test_metrics = test_metrics.iloc[:N]
+        sample_metrics = sample_metrics.iloc[:N]
+
+        linear = False
+        unbiased = True
+        bw = 'std'
+        factor = 'scott'
+
+        scale = kernels.bandwidth(test_metrics, sample_metrics, type=bw, factor=factor)
+        print("Scale:", scale)
+        mmd.test(test_metrics, sample_metrics, linear=True, unbiased=unbiased,
+                 chunksize=None, seed=seed, scale=scale)
+
+        # print("Mean: {:.6g}, std.: {:.6g}".format(*(lambda x: (np.mean(x), np.std(x)))(
+        #     [mmd.mmd2(test_metrics, sample_metrics, linear=True, seed=seed, scale=scale)[0]
+        #      for _ in range(1000)])))
+
+        print()
