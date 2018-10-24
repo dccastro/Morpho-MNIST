8f851a2f205987c8fae41a63f6f9cda37e2b304a
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2018-10-24 15:42:44 +0100

    Add documentation to morphomnist.measure

--- morphomnist/measure.py
+++ morphomnist/measure.py
@@ -6,11 +6,43 @@ import pandas as pd
 from .morpho import bounding_parallelogram, ImageMoments, ImageMorphology
 
 
-def measure_image(img: np.ndarray, threshold: float = .5, scale: int = 4, bound_frac: float = .02,
+def measure_image(image: np.ndarray, threshold: float = .5, scale: int = 4, bound_frac: float = .02,
                   verbose=True):
-    morph = ImageMorphology(img, threshold, scale)
+    """Computes morphometrics for a single image.
+
+    Parameters
+    ----------
+    image : numpy.ndarray (28, 28)
+        Input image.
+    threshold : float, optional
+        A relative threshold between 0 and 1. The upsampled image will be binarised at this fraction
+        between its minimum and maximum values.
+    scale : int, optional
+        Upscaling factor for subpixel morphological analysis (>=1).
+    bound_frac : float, optional
+        Fraction of image mass to discard along each dimension when computing the bounding
+        parallelogram.
+    verbose : bool, optional
+        Whether to pretty-print the estimated morphometrics.
+
+    Returns
+    -------
+    area : float
+        Total area/image mass.
+    length : float
+        Length of the estimated skeleton.
+    thickness : float
+        Mean thickness along the skeleton.
+    slant : float
+        Horizontal shear, in radians.
+    width : float
+        Width of the bounding parallelogram.
+    height : float
+        Height of the bounding parallelogram.
+    """
+    morph = ImageMorphology(image, threshold, scale)
     moments = ImageMoments(morph.hires_image)
-    mean_thck = morph.mean_thickness
+    thickness = morph.mean_thickness
     area = morph.area
     length = morph.stroke_length
     slant = np.arctan(-moments.horizontal_shear)
@@ -20,12 +52,13 @@ def measure_image(img: np.ndarray, threshold: float = .5, scale: int = 4, bound_
     height = (corners[-1][1] - corners[0][1]) / morph.scale
 
     if verbose:
-        print(f"Thickness: {mean_thck:.2f}")
+        print(f"Area: {area:.1f}")
         print(f"Length: {length:.1f}")
+        print(f"Thickness: {thickness:.2f}")
         print(f"Slant: {np.rad2deg(slant):.0f}°")
         print(f"Dimensions: {width:.1f} x {height:.1f}")
 
-    return area, length, mean_thck, slant, width, height
+    return area, length, thickness, slant, width, height
 
 
 def _measure_image_unpack(arg):
@@ -34,6 +67,43 @@ def _measure_image_unpack(arg):
 
 def measure_batch(images: np.ndarray, threshold: float = .5, scale: int = 4,
                   bound_frac: float = .02, pool: multiprocessing.Pool = None, chunksize: int = 100):
+    """Computes morphometrics for a batch of images.
+
+    Parameters
+    ----------
+    images : numpy.ndarray (N, 28, 28)
+        Input image batch.
+    threshold : float, optional
+        A relative threshold between 0 and 1. The upsampled image will be binarised at this fraction
+        between its minimum and maximum values.
+    scale : int, optional
+        Upscaling factor for subpixel morphological analysis (>1).
+    bound_frac : float, optional
+        Fraction of image mass to discard along each dimension when computing the bounding
+        parallelogram.
+    pool : multiprocessing.Pool, optional
+        A pool of worker processes for parallel processing. Defaults to sequential computation.
+    chunksize : int
+        Size of the chunks in which to split the batch for parallel processing. Ignored if
+        `pool=None`.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A data frame with one row for each image, containing the following columns:
+
+        - `area`: Total area/image mass.
+        - `length`: Length of the estimated skeleton.
+        - `thickness`: Mean thickness along the skeleton.
+        - `slant`: Horizontal shear, in radians.
+        - `width`: Width of the bounding parallelogram.
+        - `height`: Height of the bounding parallelogram.
+
+    Notes
+    -----
+    If the `tqdm` package is installed, this function will display a fancy progress bar with ETA.
+    Otherwise, it will print a plain text progress message.
+    """
     args = ((img, threshold, scale, bound_frac, False) for img in images)
     if pool is None:
         gen = map(_measure_image_unpack, args)
@@ -45,10 +115,8 @@ def measure_batch(images: np.ndarray, threshold: float = .5, scale: int = 4,
         gen = tqdm.tqdm(gen, total=len(images), unit='img', ascii=True)
     except ImportError:
         def plain_progress(g):
-            i = 0
             print(f"\rProcessing images: {0}/{len(images)}", end='')
-            for res in g:
-                i += 1
+            for i, res in enumerate(g):
                 print(f"\rProcessing images: {i + 1}/{len(images)}", end='')
                 yield res
             print()
