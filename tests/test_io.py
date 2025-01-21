da65b79cd5ae148182ed8ace4de5f145dda01f65
Daniel Coelho de Castro <dacoelh@microsoft.com>
2025-01-21 06:23:36 -0800

    Test also 1D labels data file

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,18 +2,22 @@ import filecmp
 from pathlib import Path
 
 import numpy as np
+import pytest
 
 from morphomnist import io
 
-SOURCE_DATA_FILE = "tests/train-images-0_3-idx3-ubyte.gz"
 
+@pytest.mark.parametrize("source_data_filename", [
+    "train-images-0_3-idx3-ubyte.gz",
+    "train-labels-0_3-idx1-ubyte.gz",
+])
+def test_load_save_idx(source_data_filename: str, tmp_path: Path) -> None:
+    source_data_path = "tests/" + source_data_filename
+    test_data_path = str(tmp_path / source_data_filename)
+    data = io.load_idx(source_data_path)
 
-def test_load_save_idx(tmp_path: Path) -> None:
-    test_data_file = str(tmp_path / "dummy-idx3-ubyte.gz")
-    data = io.load_idx(SOURCE_DATA_FILE)
+    io.save_idx(data, test_data_path)
+    filecmp.cmp(source_data_path, test_data_path, shallow=False)
 
-    io.save_idx(data, test_data_file)
-    filecmp.cmp(SOURCE_DATA_FILE, test_data_file, shallow=False)
-
-    reloaded_data = io.load_idx(test_data_file)
+    reloaded_data = io.load_idx(test_data_path)
     assert np.alltrue(data == reloaded_data)
